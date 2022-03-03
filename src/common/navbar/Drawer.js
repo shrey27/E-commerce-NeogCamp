5c60876445e0e24c87c68b1980cbe5678318f88f
Shrey Pandey <shreypandey27@gmail.com>
2022-03-03 20:41:30 +0530

    navbar added

---
+++ src/common/navbar/Drawer.js
@@ -0,0 +1,31 @@
+export default function Drawer({ open, setOpen }) {
+  return (
+    <div>
+      <div
+        class={`drawer__background ${open && 'drawer__open'}`}
+        onClick={() => setOpen(false)}
+      ></div>
+      <div class={`drawer flex-ct-ct ${open && 'drawer__open'}`}>
+        <div class='drawer__content' direction='left'>
+          <h1 class='btn--float home xs-s'>
+            <i class='fas fa-home'></i>
+          </h1>
+          <hr />
+          <section class='submenu__items flex-st-ct flex-vertical'>
+            <span class='submenu__item md sb'>Protein</span>
+            <span class='submenu__item md sb'>Gym Gear</span>
+            <span class='submenu__item md sb'>Athletics</span>
+            <span class='submenu__item md sb'>Nutrition</span>
+            <span class='submenu__item md sb'>Vegan</span>
+            <span class='submenu__item md sb'>Clothing</span>
+            <span class='submenu__item md sb'>Limited-Edition</span>
+          </section>
+          <hr />
+        </div>
+        <span class='drawer__close' onClick={() => setOpen(false)}>
+          <i class='fas fa-times-circle'></i>
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ src/common/navbar/index.js
@@ -0,0 +1,79 @@
+import './styles.css';
+import Drawer from './Drawer';
+import { useState } from 'react';
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <Drawer open={open} setOpen={setOpen} />
+      <nav class='navbar xs-s border--btm'>
+        <span class='nav__icons'>
+          <i
+            class='fas fa-bars md sb show--drawer'
+            onClick={() => setOpen(true)}
+          ></i>
+        </span>
+        <section class='start'>
+          <a href='/'>
+            <img class='box__image' src='lalogo.jpg' alt='logo' />
+          </a>
+          <span class='box-span'>
+            <span class='title primary'>LEGION ATHLETICS</span>
+          </span>
+        </section>
+        <section class='middle'>
+          <div class='search--ctr'>
+            <i class='fas fa-search search--btn'></i>
+            <input
+              type='text'
+              placeholder='Search'
+              class='input no--bdr'
+              id='user-name'
+              name='user-name'
+            />
+          </div>
+        </section>
+        <section class='end'>
+          <div class='menu'>
+            <span class='menu__btn md sb primary'>
+              <i class='far fa-user-circle'></i>
+              <span class='menu__btn__name'>Account</span>
+            </span>
+            <div class='submenu shadow xs-s'>
+              <section class='submenu__btn'>
+                <button class='btn btn--float'>Sign In</button>
+                <button class='btn'>Sign Up</button>
+              </section>
+              <section class='submenu__items flex-st-ct flex-vertical'>
+                <span class='submenu__item sb'>My Account</span>
+                <span class='submenu__item sb'>My Order</span>
+                <span class='submenu__item sb'>
+                  Cart
+                  <i
+                    icon-badge='3'
+                    bdg-size='medium'
+                    class='fas fa-shopping-cart md fl-rt'
+                  ></i>
+                </span>
+                <span class='submenu__item sb'>
+                  Wishlist
+                  <i
+                    icon-badge='3'
+                    bdg-size='medium'
+                    class='far fa-heart md fl-rt'
+                  ></i>
+                </span>
+                <span class='submenu__item sb'>Manage Account</span>
+              </section>
+              <section class='submenu__btn flex-st-ct'>
+                <button class='btn btn--float btn--wide'>Log Out</button>
+              </section>
+            </div>
+          </div>
+        </section>
+      </nav>
+    </div>
+  );
+}
